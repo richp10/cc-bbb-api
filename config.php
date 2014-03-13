bea74650b027f68480f520f19300f8733d8a3476
Matt Sprague <lodolabs@gmail.com>
2014-03-13 04:15:14 -0600

    Import current codebase

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// BASE CONFIGS (set these to match the values from your BBB server)
+/* Public test server values from Blind Side Networks:
+url: http://test-install.blindsidenetworks.com/bigbluebutton/
+salt: 8cd8ef52e8e101574e400365b55e11a6
+*/		
+define("CONFIG_SECURITY_SALT", "98477b66b94648f17b48f47b7d8f6d0d");
+define("CONFIG_SERVER_BASE_URL", "http://lv1.hostbbb.net/bigbluebutton/");
+?>
