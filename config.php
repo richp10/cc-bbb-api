289a0e266e783f8f13c17e42660831903c280449
lodolabs <lodolabs@users.noreply.github.com>
2014-03-14 01:24:45 -0600

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
 url: http://test-install.blindsidenetworks.com/bigbluebutton/
 salt: 8cd8ef52e8e101574e400365b55e11a6
 */		
-define("CONFIG_SECURITY_SALT", "98477b66b94648f17b48f47b7d8f6d0d");
-define("CONFIG_SERVER_BASE_URL", "http://lv1.hostbbb.net/bigbluebutton/");
+define("CONFIG_SECURITY_SALT", "8cd8ef52e8e101574e400365b55e11a6");
+define("CONFIG_SERVER_BASE_URL", "http://test-install.blindsidenetworks.com/bigbluebutton/");
 ?>
